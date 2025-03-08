0642b25b9c40c926f1fe250a76f09196ccf2dcb3
HarshGalav <harshsharma3122@gmail.com>
2025-03-08 18:23:18 +0530

    waitfordb added

--- app/app/test.py
+++ app/app/test.py
@@ -1,12 +1,13 @@
 from django.test import SimpleTestCase
-
 from app import calc
 
-class CalcTest(SimpleTestCase):
-    def test_add_numbers(self):
-        res=calc.add(5,6)
-        self.assertEqual(res,11)
 
-    def test_subtract_number(self):
-        res=calc.subtract(10,15)
-        self.assertEqual(res,5)
+class CalcTest(SimpleTestCase):
+
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        res = calc.subtract(15, 10)
+        self.assertEqual(res, 5)
